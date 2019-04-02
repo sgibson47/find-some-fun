a50ecb378f14dcb4a1441e4a07462cc260cf2ca5
sgibson47 <sgibson47@gmail.com>
2019-04-02 16:04:20 -0500

    add constructor function & initial state to App component; show 'Loading' if isLoaded attr of state is false

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      events: [],
+      isLoaded: false
+    }
+  }
+
   render() {
-    return (
-      <div className="App">
-        Hello World
-      </div>
-    );
+    let {events, isLoaded} = this.state
+
+    if(!isLoaded){
+      return <div>Loading</div>
+    }else{
+      return (
+        <div className="App">
+          Hello World
+        </div>
+      );
+    }
+    
   }
 }
 
