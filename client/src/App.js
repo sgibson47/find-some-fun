fa9aa285191eda26017d3f7903d755b1776f9b53
sgibson47 <sgibson47@gmail.com>
2019-04-03 18:01:50 -0500

    remove unused code

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,7 @@ import './App.css';
 import Form from './components/Form'
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state ={
-      events: []
-    }
-  }
-
-  componentDidMount(){
-    fetch('')
-  }
-
   render() {
-    let {events} = this.state
-
     return(<Form />);
   }
 }
