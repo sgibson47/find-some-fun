2aa320461e172860c981a333b5e680dab3020fde
sgibson47 <sgibson47@gmail.com>
2019-04-03 23:08:58 -0500

    tried to optionally add img if logo existed, but this just prints out a string of html, not dom objects

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
 
 class Event extends Component {  
-  render(){
-    return(
-        <div className="Event">
-            <h2>{this.props.event.name.text}</h2>
-        </div>
-    )
-  }
+    constructor(props){
+        super(props);
+    }
+
+    createEvent(event){
+        let div =`<div className="Event"><h2>${event.name.text}</h2>`
+        event.logo ? div += `<img src=${event.logo.url}/>` : div +=''
+        div+=`<p>${event.description.text}</p></div>`
+        return div
+    }
+
+    render(){
+        return(
+            this.createEvent(this.props.event)
+        )
+    }
 }
 
 export default Event;
