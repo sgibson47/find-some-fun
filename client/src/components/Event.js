757e3db378765b97adb282f9fe001d5941292f16
sgibson47 <sgibson47@gmail.com>
2019-04-03 23:29:22 -0500

    display differnt contents in Event depending on what data and event has

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -7,12 +7,45 @@ class Event extends Component {
 
     createEvent(event){
         // retun a div with name, img(if any), description (if any), and link to EB page
-        return(
-            <div className="Event">
-                <h2>{event.name.text}</h2>
-                <a href={event.url}>Tickets & More Info</a>
-            </div>
-        )
+        if(!!event.logo && !!event.description.text){
+            // if there is a logo & a description
+            return(
+                <div className="Event">
+                    <h2>{event.name.text}</h2>
+                    <img src={event.logo.url} alt="Event Logo"/>
+                    <p>{event.description.text}</p>
+                    <a href={event.url}>Tickets & More Info</a>
+                </div>
+            )
+        }else if(!event.logo && !!event.description.text){
+            //if there's a description, but no logo
+            return(
+                <div className="Event">
+                    <h2>{event.name.text}</h2>
+                    <p>{event.description.text}</p>
+                    <a href={event.url}>Tickets & More Info</a>
+                </div>
+            ) 
+        }else if(!!event.logo && !event.description.text){
+            // if there's a logo, but no description
+            return(
+                <div className="Event">
+                    <h2>{event.name.text}</h2>
+                    <img src={event.logo.url} alt="Event Logo"/>
+                    <a href={event.url}>Tickets & More Info</a>
+                </div>
+            )
+        }else{
+            //there's no logo & no description
+            return(
+                <div className="Event">
+                    <h2>{event.name.text}</h2>
+                    <a href={event.url}>Tickets & More Info</a>
+                </div>
+            )
+        }
+        
+        
     }
 
     render(){
