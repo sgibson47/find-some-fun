0b38553bae8f43f10a2e23cbf4076d04a195741c
sgibson47 <sgibson47@gmail.com>
2019-04-03 21:47:07 -0500

    create Event & EventsList components

---
+++ client/src/components/Event.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class EventsList extends Component {  
+
+  render(){
+    
+  }
+}
+
+export default Event;
---
+++ client/src/containers/EventsList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import Event from '../components/Event'
+
+class EventsList extends Component {  
+
+  render(){
+    
+  }
+}
+
+export default EventsList;
