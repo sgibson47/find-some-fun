a937354baf10d6c7e116e6457bb624ebec4ec627
sgibson47 <sgibson47@gmail.com>
2019-04-09 16:36:13 -0500

    comment out Marker & Pop up to get rid of error while I try to get a map on the page; give Map a class of 'map' and give map class a height of 40% in App.css

--- src/components/MyMap.js
+++ src/components/MyMap.js
@@ -6,24 +6,24 @@ class MyMap extends Component{
         super(props)
         this.state = {
             lat: 51.505,
-            long: -0.09,
+            lng: -0.09,
             zoom: 13
         }
     }
 
     render() {
-        const position = [this.state.lat, this.state.long]
+        const position = [this.state.lat, this.state.lng]
         return (
-            <Map center={position} zoom={this.state.zoom} className="map">
+            <Map class='map' center={position} zoom={this.state.zoom}>
                 <TileLayer
                     attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={position}>
+                {/* <Marker position={position}>
                     <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
-                </Marker>
+                </Marker> */}
             </Map>
         )
     }
