5fb558a819b5058d6a3c86b15851700646459cfd
sgibson47 <sgibson47@gmail.com>
2019-04-09 15:58:16 -0500

    add constructor with inital state

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,14 @@
 import React, {Component} from 'react';
 
 class Map extends Component{
-    
-
+    constructor(props){
+        super(props)
+        this.state = {
+            lat: 51.505,
+            long: -0.09,
+            zoom: 13
+        }
+    }
 
 }
 
