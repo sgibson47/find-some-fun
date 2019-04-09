5a3ece9267b57aadcd6f935e3378c4d03b86bb9d
sgibson47 <sgibson47@gmail.com>
2019-04-09 16:20:06 -0500

    give Map a className attr & give same class height in App.css

--- src/components/MyMap.js
+++ src/components/MyMap.js
@@ -14,16 +14,16 @@ class MyMap extends Component{
     render() {
         const position = [this.state.lat, this.state.long]
         return (
-            <Map center={position} zoom={this.state.zoom}>
-            <TileLayer
-                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={position}>
-                <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+            <Map center={position} zoom={this.state.zoom} className="map">
+                <TileLayer
+                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position}>
+                    <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
             </Map>
         )
     }
